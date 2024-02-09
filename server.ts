02fb388ffca235d6bf037c5a226e670e9ef92d3f
William Fontaine <william.fontaine0308@gmail.com>
2024-02-09 16:01:17 +0100

    Broken Access Control

--- server.ts
+++ server.ts
@@ -336,7 +336,10 @@ restoreOverwrittenFilesWithOriginals().then(() => {
     .delete(security.denyAll())
   /* Products: Only GET is allowed in order to view products */ // vuln-code-snippet neutral-line changeProductChallenge
   app.post('/api/Products', security.isAuthorized()) // vuln-code-snippet neutral-line changeProductChallenge
-  // app.put('/api/Products/:id', security.isAuthorized()) // vuln-code-snippet vuln-line changeProductChallenge
+  app.put('/api/Products/:id')
+    .get(security.isAuthorized())
+    .post(security.denyAll())
+    .delete(security.denyAll())
   app.delete('/api/Products/:id', security.denyAll())
   /* Challenges: GET list of challenges allowed. Everything else forbidden entirely */
   app.post('/api/Challenges', security.denyAll())
